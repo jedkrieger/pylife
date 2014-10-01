5c811ffea377490c8da660f4b09247f36b19381d
Jed Krieger <jedkrieger@gmail.com>
2014-10-01 12:04:04 -0500

    v2

--- fizzbuzz_v2.py
+++ fizzbuzz_v2.py
@@ -1,14 +1,22 @@
-n = raw_input('please enter a number you would like to count to: ')
+while True:
+	try:
+		n = int(raw_input('please enter a number you would like to count to: '))
+		break
+	except:
+		print 'you need to enter a number, please'
 
-print "fizz buzz counting up to"+n
+print "fizz buzz counting up to "+str(n)
 
-n = int(n) + 1
-for n in range(1, n):
-	if n%3 == 0 and n%5 == 0:
+n = n + 1
+for x in range(1, n):
+	if x%3 == 0 and x%5 == 0:
 		print 'fizz buzz'
-	elif n%3 == 0:
+	elif x%3 == 0:
 		print 'fizz'
-	elif n%5 == 0:
+	elif x%5 == 0:
 		print 'buzz'
 	else:
-		print n
+		print x
+
+		# use specific exception on line 5
+		# another way to handle the while/try/except block
